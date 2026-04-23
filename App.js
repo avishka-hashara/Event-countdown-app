d61bf574444afbf4e8000c04b576b0adf3b66793
Avishka <eaahashara01@gmail.com>
2026-04-23 22:23:50 +0530

    refactor: migrate from expo-router to react-navigation stack navigator

---
+++ App.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import Views
+import AddEventScreen from './src/views/AddEventScreen';
+import EditEventScreen from './src/views/EditEventScreen';
+import EventDetailScreen from './src/views/EventDetailScreen';
+import HomeScreen from './src/views/HomeScreen';
+import SettingsScreen from './src/views/SettingsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'My Events' }} />
+                <Stack.Screen name="AddEvent" component={AddEventScreen} options={{ title: 'New Event' }} />
+                <Stack.Screen name="EventDetail" component={EventDetailScreen} options={{ title: 'Countdown' }} />
+                <Stack.Screen name="EditEvent" component={EditEventScreen} options={{ title: 'Edit Event' }} />
+                <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
